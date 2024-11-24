117c92b00f017d6b69f9f01e21aff18f7f8f2001
GuusDb <79090456+GuusDb@users.noreply.github.com>
2024-11-24 13:54:00 +0100

    code cleanup

--- opencvforWindows/Chapter7.cpp
+++ opencvforWindows/Chapter7.cpp
@@ -25,29 +25,31 @@ void getContours(Mat imgDil, Mat img) {
 		cout << area << endl;
 		string objectType;
 
-		if (area > 1000)
+		if (area < 1000)
 		{
-			float peri = arcLength(contours[i], true);
-			approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
-			cout << conPoly[i].size() << endl;
-			boundRect[i] = boundingRect(conPoly[i]);
-
-			int objCor = (int)conPoly[i].size();
-
-			if (objCor == 3) { objectType = "Tri"; }
-			else if (objCor == 4)
-			{
-				float aspRatio = (float)boundRect[i].width / (float)boundRect[i].height;
-				cout << aspRatio << endl;
-				if (aspRatio > 0.95 && aspRatio < 1.05) { objectType = "Square"; }
-				else { objectType = "Rect"; }
-			}
-			else if (objCor > 4) { objectType = "Circle"; }
-
-			drawContours(img, conPoly, i, Scalar(255, 0, 255), 2);
-			rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 5);
-			putText(img, objectType, { boundRect[i].x,boundRect[i].y - 5 }, FONT_HERSHEY_PLAIN, 1, Scalar(0, 69, 255), 2);
+			continue;
 		}
+		float peri = arcLength(contours[i], true);
+		approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
+		cout << conPoly[i].size() << endl;
+		boundRect[i] = boundingRect(conPoly[i]);
+
+		int objCor = (int)conPoly[i].size();
+
+		if (objCor == 3) { objectType = "Tri"; }
+		else if (objCor == 4)
+		{
+			float aspRatio = (float)boundRect[i].width / (float)boundRect[i].height;
+			cout << aspRatio << endl;
+			if (aspRatio > 0.95 && aspRatio < 1.05) { objectType = "Square"; }
+			else { objectType = "Rect"; }
+		}
+		else if (objCor > 4) { objectType = "Circle"; }
+
+		drawContours(img, conPoly, i, Scalar(255, 0, 255), 2);
+		rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 5);
+		putText(img, objectType, { boundRect[i].x,boundRect[i].y - 5 }, FONT_HERSHEY_PLAIN, 1, Scalar(0, 69, 255), 2);
+
 	}
 }
 
